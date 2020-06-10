48aa8b593a41361ec9381e96e8cff819d4783ce3
Rupert Rutledge <1982481+Eosis@users.noreply.github.com>
2020-06-10 21:28:41 +0100

    First commit of a game of Snake.
    - Defined different structs for different parts of the game, Snakes,
      Apples and the Game itself.
    - Include render function for rendering these to the board, and a helper
      to render the board with a border.
    - Includes one piecemeal test to remind myself of the syntax for writing
      tests. 😅

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::error::Error;
+
+struct Game {
+    pub snake: Snake,
+    pub apples: Vec<Apple>,
+    pub width: usize,
+    height: usize,
+    rendered: Vec<Vec<char>>,
+}
+
+struct Snake {
+    head: (usize, usize),
+    body: Vec<(usize, usize)>,
+}
+
+struct Apple {
+    location: (usize, usize),
+}
+
+impl Game {
+    pub fn new(width: usize, height: usize) -> Game {
+        Game {
+            snake: Snake {
+                head: (10, 10),
+                body: vec![(10, 10), (10, 9), (10, 8), (10, 7), (10, 6)],
+            },
+            apples: vec![],
+            rendered: vec![vec![' '; width]; height],
+            width,
+            height,
+        }
+    }
+
+    pub fn render_with_border(&mut self) {
+        self.render();
+        println!("{}", "-".repeat(self.width + 2));
+        for row in self.rendered.iter() {
+            println!("|{}|", row.iter().collect::<String>());
+        }
+        println!("{}", "-".repeat(self.width + 2));
+    }
+
+    fn render(&mut self) {
+        self.render_snake();
+        self.render_apples();
+    }
+
+    fn render_snake(&mut self) {
+        let (y, x) = self.snake.head;
+        self.rendered[y][x] = 'X';
+        for point in self.snake.body.iter().skip(1) {
+            let (y, x) = point;
+            self.rendered[*y][*x] = '=';
+        }
+    }
+
+    fn render_apples(&mut self) {
+        for apple in &self.apples {
+            let (y, x) = apple.location;
+            self.rendered[y][x] = 'O';
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut game = Game::new(20, 20);
+    game.apples = vec![Apple { location: (4, 4) }];
+    game.render_with_border();
+    Ok(())
+}
+
+#[cfg(test)]
+fn count_board_squares(game: Game) -> usize {
+    game.rendered.iter().fold(0, |tot, row| tot + row.len())
+}
+
+#[test]
+fn test_creating_boards() {
+    assert_eq!(count_board_squares(Game::new(20, 20)), 20 * 20);
+    assert_eq!(count_board_squares(Game::new(50, 1)), 50);
+}
