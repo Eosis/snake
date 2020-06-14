510781c6bed8e340b244570eef288e0b86df644f
Rupert Rutledge <1982481+Eosis@users.noreply.github.com>
2020-06-14 22:44:31 +0100

    Writing logic for drawing more complex snakes
    Currently only horizontal and vertical snakes are tested, with more
    complex snakings to be tested later.

--- src/main.rs
+++ src/main.rs
@@ -67,13 +67,50 @@ impl Game {
     }
 
     fn render_snake(&mut self) {
-        let mut body_iter = self.snake.body.iter();
-        let (y, x) = body_iter.next().unwrap();
-        self.rendered[*y][*x] = 'X';
-        for point in body_iter {
-            let (y, x) = point;
-            self.rendered[*y][*x] = '=';
+        self.render_snake_head();
+        self.render_snake_body();
+        self.render_snake_tail();
+    }
+
+    fn render_snake_head(&mut self) {
+        let direction = self.snake.head_direction();
+        let (y, x) = self.snake.body[0];
+        let glyph = match direction {
+            Direction::Up => '^',
+            Direction::Right => '>',
+            Direction::Down => 'v',
+            Direction::Left => '<',
+        };
+        self.rendered[y][x] = glyph;
+    }
+
+    fn render_snake_body(&mut self) {
+        for window in Vec::from(self.snake.body.clone()).windows(3) {
+            let to = Snake::direction(window[0], window[1]);
+            let from = Snake::direction(window[1], window[2]);
+            let joining_glyph = Snake::get_body_glyph_from_directions(to, from);
+            let (y_to_set, x_to_set) = window[1];
+            self.rendered[y_to_set][x_to_set] = joining_glyph;
         }
+    }
+
+    fn render_snake_tail(&mut self) {
+        let relevant_points: Vec<_> = self
+            .snake
+            .body
+            .iter()
+            .rev()
+            .take(2)
+            .rev()
+            .cloned()
+            .collect();
+        let direction = Snake::direction(relevant_points[0], relevant_points[1]);
+        let (y, x) = self.snake.body.back().unwrap();
+        let glyph = match direction {
+            Direction::Up | Direction::Down => '║',
+            Direction::Right | Direction::Left => '=',
+        };
+        self.rendered[*y][*x] = glyph;
     }
 
     fn render_apples(&mut self) {
@@ -107,7 +144,7 @@ impl Snake {
     }
 
     pub fn advance(&mut self) {
-        let direction = self.direction();
+        let direction = self.head_direction();
         let (dy, dx) = Snake::advancement_to_add(direction);
         let first = self.body.front().unwrap();
         let (y, x) = (first.0 as i32, first.1 as i32);
@@ -131,6 +168,27 @@ impl Snake {
     fn dead_at(&self, (y, x): (i32, i32)) -> bool {
         y < 0 || x < 0 || y >= self.confines.0 as i32 || x >= self.confines.1 as i32
     }
+
+    fn get_body_glyph_from_directions(to: Direction, from: Direction) -> char {
+        match (to, from) {
+            (Direction::Up, Direction::Up) => '║',
+            (Direction::Up, Direction::Right) => '╝',
+            (Direction::Up, Direction::Down) => panic!("Not possible"),
+            (Direction::Up, Direction::Left) => '╚',
+            (Direction::Right, Direction::Up) => '╔',
+            (Direction::Right, Direction::Right) => '=',
+            (Direction::Right, Direction::Down) => '╚',
+            (Direction::Right, Direction::Left) => panic!("Not possible"),
+            (Direction::Down, Direction::Up) => panic!("Not possible"),
+            (Direction::Down, Direction::Right) => '╗',
+            (Direction::Down, Direction::Down) => '║',
+            (Direction::Down, Direction::Left) => '╔',
+            (Direction::Left, Direction::Up) => '╗',
+            (Direction::Left, Direction::Right) => panic!("Not possible"),
+            (Direction::Left, Direction::Down) => '╝',
+            (Direction::Left, Direction::Left) => '=',
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -142,10 +200,14 @@ enum Direction {
 }
 
 impl Snake {
-    fn direction(&self) -> Direction {
-        let head = Self::point_helper(self.body[0]);
-        let second = Self::point_helper(self.body[1]);
-        match (head.0 - second.0, head.1 - second.1) {
+    fn head_direction(&self) -> Direction {
+        Snake::direction(self.body[0], self.body[1])
+    }
+
+    fn direction((now_y, now_x): (usize, usize), (then_y, then_x): (usize, usize)) -> Direction {
+        let (now_y, now_x) = Self::point_helper((now_y, now_x));
+        let (then_y, then_x) = Self::point_helper((then_y, then_x));
+        match (now_y - then_y, now_x - then_x) {
             (-1, 0) => Direction::Up,
             (1, 0) => Direction::Down,
             (0, 1) => Direction::Right,
@@ -208,17 +270,20 @@ fn test_direction() {
     let downwards_body = vec![(11, 10), (10, 10)];
     let rightwards_body = vec![(10, 11), (10, 10)];
     let leftwards_body = vec![(10, 9), (10, 10)];
-    assert_eq!(Snake::from_body(&upwards_body).direction(), Direction::Up);
     assert_eq!(
-        Snake::from_body(&downwards_body).direction(),
+        Snake::from_body(&upwards_body).head_direction(),
+        Direction::Up
+    );
+    assert_eq!(
+        Snake::from_body(&downwards_body).head_direction(),
         Direction::Down
     );
     assert_eq!(
-        Snake::from_body(&rightwards_body).direction(),
+        Snake::from_body(&rightwards_body).head_direction(),
         Direction::Right
     );
     assert_eq!(
-        Snake::from_body(&leftwards_body).direction(),
+        Snake::from_body(&leftwards_body).head_direction(),
         Direction::Left
     );
 }
@@ -226,9 +291,21 @@ fn test_direction() {
 #[test]
 fn test_drawing_correct_snakes() {
     let correct_upwards = concat!(" ^ \n", " ║ \n", " ║ \n");
-    let _correct_rightwards = concat!("   \n", "==>\n", "   \n");
-    let _correct_leftwards = concat!("   \n", "<==\n", "   \n");
-    let _correct_downwards = concat!(" ║ \n", " ║ \n", " v \n");
-    let mut game = Game::new(3, 3, &[(0, 1), (1, 1), (2, 1)]);
-    assert_eq!(game.render_to_string(), correct_upwards);
+    let correct_rightwards = concat!("   \n", "==>\n", "   \n");
+    let correct_leftwards = concat!("   \n", "<==\n", "   \n");
+    let correct_downwards = concat!(" ║ \n", " ║ \n", " v \n");
+    let mut upwards_game = Game::new(3, 3, &[(0, 1), (1, 1), (2, 1)]);
+    assert_eq!(upwards_game.render_to_string(), correct_upwards);
+
+    let mut correct_rightwards_game = Game::new(3, 3, &[(1, 2), (1, 1), (1, 0)]);
+    assert_eq!(
+        correct_rightwards_game.render_to_string(),
+        correct_rightwards
+    );
+
+    let mut leftwards_game = Game::new(3, 3, &[(1, 0), (1, 1), (1, 2)]);
+    assert_eq!(leftwards_game.render_to_string(), correct_leftwards);
+
+    let mut downwards_game = Game::new(3, 3, &[(2, 1), (1, 1), (0, 1)]);
+    assert_eq!(downwards_game.render_to_string(), correct_downwards);
 }
